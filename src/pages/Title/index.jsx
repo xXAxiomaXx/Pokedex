04130fcc9d0bc642d8f124ad37efe44a2e07233c
Eric Moura <100503308+xXAxiomaXx@users.noreply.github.com>
2025-04-24 19:46:05 -0300

    Add files via upload

--- src/pages/Title/index.jsx
+++ src/pages/Title/index.jsx
@@ -3,21 +3,24 @@ import background from "../../assets/Images/mylivewallpapers-com-Eevee-Pokemon-4
 import backgroundMobile from "../../assets/Images/MOBILE-Eevee-Pokemon.mp4";
 import Logo from "../../assets/Images/612ce4761b9679000402af1c.png";
 
-// 
+//
 const Title = () => {
-
   return (
     <>
-      <section className="justify-center overflow-hidden items-center h-screen w-screen">
-        <nav className="fixed w-screen z-50 px-6 py-4">
-          <img src={Logo} alt="Pokémon Logo" className="w-2/6 lg:w-1/8 z-50" />
-        </nav>
-        <div className="h-screen w-screen flex flex-col justify-around items-center py-10">
+      <section className="flex justify-center overflow-hidden items-center h-screen w-screen">
+        <div className="h-screen w-screen flex flex-col justify-between items-center py-10">
+          <nav className=" w-screen z-50 px-6 py-4">
+            <img
+              src={Logo}
+              alt="Pokémon Logo"
+              className="w-2/6 lg:w-1/8 z-50"
+            />
+          </nav>
           <video
             autoPlay
             loop
             muted
-            className="bg-vid hidden lg:block fixed w-full h-full object-cover z-0"
+            className="bg-vid hidden lg:block absolute top-0 left-0 w-full h-screen object-cover z-0"
           >
             <source src={background} type="video/mp4" />
           </video>
@@ -25,14 +28,16 @@ const Title = () => {
             autoPlay
             loop
             muted
-            className="bg-vid fixed block lg:hidden w-full h-full object-cover z-0"
+            className="bg-vid absolute top-0 left-0 block lg:hidden w-full h-full object-cover z-0"
           >
             <source src={backgroundMobile} type="video/mp4" />
           </video>
           <div className="shadow"></div>
           <div className="flex flex-col justify-around h-full w-full items-center gap-5 z-50">
             <div className="flex flex-col justify-center w-8/12 text-center gap-5 text-white items-center">
-              <h1 className="text-7xl sm:text-8xl font-bold font-outline-2">Ready to explore the Pokémon's world??</h1>
+              <h1 className="text-7xl sm:text-8xl font-bold font-outline-2">
+                Ready to explore the Pokémon's world??
+              </h1>
             </div>
             <a
               className="text-white text-2xl z-50 bg-black hover:bg-white hover:text-black hover:border-black duration-150 ease-in-out py-2 px-5 rounded-xl border font-bold"
@@ -41,7 +46,7 @@ const Title = () => {
               Pokémon List
             </a>
           </div>
-          <footer className="z-50 w-screen absolute bottom-2 flex flex-col items-center">
+          <footer className="z-50 w-screen flex flex-col items-center">
             <hr className="my-6 border-gray-200 w-11/12 lg:w-8/12 sm:mx-auto lg:my-4" />
             <span className="flex items-center justify-center gap-2 text-sm text-gray-500 sm:text-center">
               © 2025
