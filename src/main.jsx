15d9fbc3b3d1aba416c9517d5e6baed702525c12
Eric Moura <100503308+xXAxiomaXx@users.noreply.github.com>
2025-04-24 16:49:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Pokemon from "./pages/pokemon/index.jsx";
+import Pokemon from "./pages/Pokemon/index.jsx";
 import ListaPokemons from "./pages/ListaPokemons/index.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
